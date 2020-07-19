807d4232bde6bd5c7f824dad14dcec37be4a6c42
Ethan Dowler <ethan.dowler@daveramsey.com>
2020-07-19 15:15:04 -0500

    init basic cards

--- deck.rb
+++ deck.rb
@@ -1,6 +1,35 @@
+require 'active_model'
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+require_relative "models/card"
+
+# icons from game-icons.net
+CARDS = [
+  Card.new(
+    title: "Strike",
+    image: "images/guardian/battle-axe.svg",
+    body:  "Deal 3 damage."
+  ),
+  Card.new(
+    title: "Block",
+    image: "images/guardian/round-shield.svg",
+    body:  "Prevent 2 damage."
+  )
+]
+
+Squib::Deck.new(cards: 2) do
+  background color: '#FFFFFF'
+
+  text str: CARDS.map(&:title),
+       color: '#000000',
+       font: 'ChunkFive Roman,Sans 24',
+       y: '2.5in',
+       width: '2.75in',
+       align: :center
+
+  svg file: CARDS.map(&:image),
+      width: 500, height: 500,
+      x: 150, y: 250
+
+  save_png
 end
