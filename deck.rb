b114da7e8bd767e4b3b0a8c2c3893dc33abe1ffb
Ethan Dowler <ethan.dowler@daveramsey.com>
2020-07-25 00:05:36 -0500

    add space for bleed

--- deck.rb
+++ deck.rb
@@ -1,9 +1,11 @@
-require 'active_model'
-require 'squib'
+require "active_model"
+require "game_icons"
+require "squib"
 
 require_relative "models/card"
 
 # icons from game-icons.net
+# TODO: make this work with Guard
 CARDS = [
   Card.new(
     title: "Strike",
@@ -17,19 +19,37 @@ CARDS = [
   )
 ]
 
-Squib::Deck.new(cards: 2) do
-  background color: '#FFFFFF'
+deck_dsl = Squib::Deck.new
+card_width = deck_dsl.inches(2.5)
+card_height = deck_dsl.inches(3.5)
+bleed = deck_dsl.inches(0.125)
+
+template_width = card_width + (bleed * 2)
+template_height = card_height + (bleed * 2)
+
+Squib::Deck.new(cards: CARDS.length, width: template_width, height: template_height) do
+  background color: :white
+
+  rect x: bleed,
+       y: bleed,
+       width: card_width,
+       height: card_height,
+       dash: 5
 
   text str: CARDS.map(&:title),
-       color: '#000000',
+       color: :black,
        font: 'ChunkFive Roman,Sans 24',
-       y: '2.5in',
-       width: '2.75in',
+       x: bleed,
+       y: bleed + 100,
+       width: card_width,
        align: :center
 
+  image_size = inches(1.5)
   svg file: CARDS.map(&:image),
-      width: 500, height: 500,
-      x: 150, y: 250
+    width: image_size,
+    height: image_size,
+    x: bleed + (card_width - image_size) / 2,
+    y: bleed + inches(0.65)
 
   save_png
 end
